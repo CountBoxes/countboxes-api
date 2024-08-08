44f61d83c0e4887d203a30cf1a12aa8db4279eeb
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2024-08-08 18:48:24 -0300

    feat: fix code structures

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,20 +1,24 @@
 import { createUser, getAll } from "../repositories/User";
 import { userValidation } from "../validations/User";
-export const create = async(req, res) => {
-    try {
-        await userValidation.validate(req.body);
-        const user = await createUser(req.body);
-        res.status(200).send(user);
-    } catch(err) {
-        res.status(400).send(err);
-    }
-}
+import CreateUserService from "../services/User/CreateUserService";
 
-export const get = async(req,res) => {
-    try {
-        const users = await getAll();
-        res.status(200).send(users);
-    } catch(err) {
-        res.status(400).send(err);
-    }
-}
+export const create = async (req, res) => {
+  try {
+    const data = await userValidation.validate(req.body);
+
+    const user = CreateUserService.execute(data);
+
+    res.status(200).send(user);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+export const get = async (req, res) => {
+  try {
+    const users = await getAll();
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
--- src/repositories/User.js
+++ src/repositories/User.js
@@ -1,13 +1,22 @@
-import { prisma } from "../services/prisma";
+import prisma from "../../prisma/client";
 
 export const createUser = async (data) => {
-    const user = await prisma.user.create({
-        data,
-    });
-    return user;
+  const user = await prisma.user.create({
+    data,
+  });
+  return user;
 };
 
 export const getAll = async () => {
-    const users = await prisma.user.findMany({});
-    return users;
-}
+  const users = await prisma.user.findMany({});
+  return users;
+};
+
+export const getByEmail = async (email) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  return user;
+};
