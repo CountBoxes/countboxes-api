deb267017221566532905e65c868ead542be662c
akiwnl <carvalhokaike69@gmail.com>
2024-08-12 00:20:30 -0300

    add swagger

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import express, { json } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import routes from './routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger.json'
+
 dotenv.config()
 
 const app = express()
 
 app.use(cors())
 app.use(json())
-
 app.use(routes)
+app.use("/docs", swaggerUi.serve,swaggerUi.setup(swaggerDocument)) 
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
