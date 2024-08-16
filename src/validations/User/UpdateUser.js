89700eecb05a6cf5af6d0ea215d4d11261b15282
akiwnl <carvalhokaike69@gmail.com>
2024-08-16 14:37:36 -0300

    refactor update user

--- src/validations/User/UpdateUser.js
+++ src/validations/User/UpdateUser.js
@@ -3,19 +3,14 @@ import * as yup from 'yup'
 const phoneRegex = /^\+?[1-9]\d{1,14}$/
 
 export const UpdateUserSchema = yup.object({
-    name: yup.string().optional(),
-    phone: yup
+    name: yup.string(),
+    phone: yup.string().matches(phoneRegex, 'Número de telefone inválido').optional(),
+    type: yup.string(),
+    active: yup
+        .boolean(),
+    email: yup.string().email('Email inválido'),
+    password: yup.string().min(6, 'Senha deve ter pelo menos 6 caracteres'),
+    confirmPassword: yup
         .string()
-        .matches(phoneRegex, 'Número de telefone inválido')
-        .optional(),
-    password: yup
-        .string()
-        .min(6, 'Senha deve ter pelo menos 6 caracteres')
-        .optional(),
-    type: yup
-        .string()
-        .optional(),
-    status: yup
-        .string()
-        .optional()
-})
+        .oneOf([yup.ref('password'), null], 'As senhas devem coincidir'),
+});
